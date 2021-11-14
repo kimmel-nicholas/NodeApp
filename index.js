3f6c3b0fe4efc1766a058e982918587437dc0908
kimmel-nicholas <kimmel_nicholas@columbusstate.edu>
2021-11-14 19:06:19 +0000

    just a little extra

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded( {exetend:true} ))
+
+
+app.get('/', function(request, response){
+    response.render('index');
+    //response.send('Hello World!');
+})
+
+app.post('/addToDo', function(req, res){
+    res.send('hello')
+})
+
+app.post('/removeToDo', function(req, res){
+    res.send('world')
+})
+
+app.listen(3000, function(){
+    console.log('App is running on port 3000')
+})
